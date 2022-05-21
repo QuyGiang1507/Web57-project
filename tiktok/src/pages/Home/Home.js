4785ee540cd4a46eb1578a36a25a01db766c2adc
QuyGiang1507 <phungminhquygiang@gmail.com>
2022-05-21 15:54:14 +0700

    bug map posts

--- tiktok/src/pages/Home/Home.js
+++ tiktok/src/pages/Home/Home.js
@@ -7,18 +7,16 @@ import './Home.css'
 
 const MAX_LENGTH = 3;
 
-function MainPage () {
+function Home () {
     const [postData, setPostData] = useState({
         state: 'idle',
-        data: null,
+        data: [],
     })
 
     const [ activeData, setActiveData ] = useState ({
         skip: 1,
         limit: MAX_LENGTH,
     })
-
-    const [ more, setMore ] = useState(true);
 
     const fetchPosts = useCallback(async () => {
         try {
@@ -27,51 +25,59 @@ function MainPage () {
                 status: "loading",
             }));
             const res = await axios.get(`http://localhost:9000/api/posts/?limit=${activeData.limit}&skip=${activeData.skip}`)
+            
+            console.log(res)
             if (res.data.success) {
-                setPostData({
+                setPostData((preState) => ({
+                    ...preState,
                     status: "success",
-                    data: {
-                        posts: res.data.data.enhanceReactionPosts,
-                        total: res.data.data.totalPosts,
-                    },
-                });
+                    data:  res.data.data.reaction,
+                }));
             } else {
                 setPostData((preState) => ({
                     ...preState,
                     status: "error",
                 }));
             }
+            
         } catch (err) {
             setPostData((preState) => ({
             ...preState,
             status: "error",
             }));
         }
+        
     }, [activeData]);
-
+    
+    
     useEffect(() => {
         fetchPosts();
     }, [fetchPosts])
 
-    function fetchMorePosts() {
-        setActiveData((preState) => ({
-            ...preState,
-            skip: preState.skip + 1,
-            limit: MAX_LENGTH * preState.skip,
-        }))
-    }
+    
+    // const loadFunc = async () => {
+    //     setActiveData((preState) => ({
+    //         ...preState,
+    //         skip: preState.skip + 1,
+    //     }))
+    // }
 
     return (
         <div className="main">
             <Header />
             <div className="video__container">
-                <InfiniteScroll
-                    dataLength={MAX_LENGTH}
-                    next={fetchMorePosts}
-                    hasMore={more}
-                    loader={<h4>Loading...</h4>}
+                {/*<InfiniteScroll
+                    pageStart={0}
+                    loadMore={loadFunc}
+                    hasMore={true || false}
+                    useWindow={false}
+                    loader={
+                    <div key="loading" className="loader">
+                        Loading ...
+                    </div>
+                    }
                 >
-                    {postData.data.posts.map((post) => (
+                    {postData.data.map((post) => (
                         <Video 
                             key={post._id}
                             url={post.url}
@@ -82,10 +88,22 @@ function MainPage () {
                             messages={post.commentCount}
                         />
                     ))}
-                </InfiniteScroll>
+                </InfiniteScroll>*/}
+                {console.log(postData.data)}
+                {postData.data.map((post) => (
+                    <Video 
+                        key={post._id}
+                        url={post.url}
+                        channel={post.createdBy}
+                        description={post.description}
+                        song={post.song}
+                        likes={post.likeCount}
+                        messages={post.commentCount}
+                    />
+                ))}
             </div>
         </div>
     )
 }
 
-export default MainPage
+export default Home
